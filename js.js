38d9a75d5ffebebbcff8740f91ad84ef26014cac
sorrowlol <sorrowlol@outlook.com>
2021-06-20 15:22:00 +0200

    new

--- js.js
+++ js.js
@@ -12,53 +12,130 @@ function computerPlay(){
 return a;
 }
 
+let playerwin = 0, computerwin = 0;
+
 
 function round(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerPlay();
 
     if (playerSelection === "rock"){
-        if (computerSelection === "Rock"){
-            return `It's a draw! Both chose ${computerSelection}`;
-        }
-        else if (computerSelection === "Paper"){
-            return `You Lose! Computer chose ${computerSelection}`;
+
+        if (computerSelection === "Paper"){
+            disply.textContent = `You Lose! Computer chose ${computerSelection}`
+            computerwin++;
+            computersc.textContent = computerwin;
         }
         else if (computerSelection === "Scissors"){
-            return `You Win! Computer chose ${computerSelection}`;
+            disply.textContent = `You Win! Computer chose ${computerSelection}`;
+            playerwin++;
+            playersc.textContent = playerwin;
         }
+        else if (computerSelection === "Rock"){
+            disply.textContent = `It's a draw! Both chose ${computerSelection}`;
+        }
+
     }
 
     else if (playerSelection === "paper"){
         if (computerSelection === "Rock"){
-            return `You Win! Computer chose ${computerSelection}`;
-        }
-        else if (computerSelection === "Paper"){
-            return `It's a draw! Both chose ${computerSelection}`;
+            disply.textContent = `You Win! Computer chose ${computerSelection}`;
+            playerwin++;
+            playersc.textContent = playerwin;
         }
         else if (computerSelection === "Scissors"){
-            return `You Lose! Computer chose ${computerSelection}`;
+            disply.textContent = `You Lose! Computer chose ${computerSelection}`;
+            computerwin++;
+            computersc.textContent = computerwin;
         }
+        else if (computerSelection === "Paper"){
+            disply.textContent = `It's a draw! Both chose ${computerSelection}`;
+        }
+        
     }
 
     else if (playerSelection === "scissors"){
         if (computerSelection === "Rock"){
-            return `You Lose! Computer chose ${computerSelection}`;
+            disply.textContent = `You Lose! Computer chose ${computerSelection}`;
+            computerwin++;
+            computersc.textContent = computerwin;
         }
         else if (computerSelection === "Paper"){
-            return `You Win! Computer chose ${computerSelection}`;
+            disply.textContent = `You Win! Computer chose ${computerSelection}`;
+            playerwin++;
+            playersc.textContent = playerwin;
         }
         else if (computerSelection === "Scissors"){
-            return `It's a Draw! Both chose ${computerSelection}`;
+            console.log(`It's a draw! Both chose ${computerSelection}`);
         }
     }
+    
+       if (computerwin === 5){
+        cret.textContent = `Machines always win, with ${computerwin} vs your measly ${playerwin}`;
+        winner.appendChild(cret);
+        computerwin = 0;
+        playerwin = 0;
+    }
+    
+    else if (playerwin === 5){
+        cret.textContent = `You win! with ${playerwin} vs the stupid machines ${computerwin}`;
+        winner.appendChild(cret);
+        computerwin = 0;
+        playerwin = 0;
+    }
+
 }
 
-function game(){
-    let a = prompt("choose your thing",);
-    console.log(round(a));
-    console.log(round(a));
-    console.log(round(a));
-    console.log(round(a));
-    console.log(round(a));
+/*
+function game() {
+    for (i = 0; i < 5; i++){                                        //iterates through 5 rounds
+        console.log(round(prompt("Choose scissors paper or rock bruv"), computerPlay));
+    }
+    if (computerwin > playerwin){
+        console.log(`comp wins :( with ${computerwin} vs your ${playerwin}`);
+    }
+    else if (playerwin > computerwin){
+        console.log(`player wins :) with ${playerwin} vs computer ${computerwin}`);
+    }
+    else {
+        console.log(`both tie with ${computerwin} and ${playerwin}`);
+    }
+
 }
+*/
+
+
+//nodelist
+
+const resultsjs = document.querySelector('#results');
+const disply = document.createElement('p');
+resultsjs.appendChild(disply);
+
+document.getElementById('scissors').addEventListener('click', () => {
+    console.log(round(document.getElementById('scissors').id, computerPlay));
+})
+
+document.getElementById('paper').addEventListener('click', () => {
+    console.log(round(document.getElementById('paper').id, computerPlay));
+})
+
+document.getElementById('rock').addEventListener('click', () => {
+    console.log(round(document.getElementById('rock').id, computerPlay));
+})
+
+//player score
+const scorejs = document.querySelector('.player-score');
+const playersc = document.createElement('p');
+scorejs.appendChild(playersc);
+
+//computer score
+
+const compscorejs = document.querySelector('.computer-score');
+const computersc = document.createElement('p');
+compscorejs.appendChild(computersc);
+
+const winner = document.getElementById('winner');
+const cret = document.createElement('p');
+
+computersc.textContent = 0;
+playersc.textContent = 0;
